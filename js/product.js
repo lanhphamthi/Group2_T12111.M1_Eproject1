8c47b03d706654258f49781382191c1e62d921e1
lanhphamthi <56110791+lanhphamthi@users.noreply.github.com>
2022-03-15 10:45:58 +0700

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,68 @@
+var data = 
+[
+    {"id": "P01", "name": "Lotus Pink", "price": 100, "pic": "lotus_pink.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat":"lotus"},
+    {"id": "P02", "name": "Lotus White", "price": 100, "pic": "lotus_white.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "lotus"},
+    {"id": "P11", "name": "Orchid Purple", "price": 100, "pic": "orchid_purple.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "orchid"},
+    {"id": "P12", "name": "Orchid Red", "price": 100, "pic": "orchid_red.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "orchid"},
+    {"id": "P13", "name": "Orchid White", "price": 100, "pic": "orchid_white.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "orchid"},
+    {"id": "P14", "name": "Orchid Yellow", "price": 100, "pic": "orchid_yellow.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "orchid"},
+    {"id": "P21", "name": "Rose Blue", "price": 100, "pic": "rose_blue.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "rose"},
+    {"id": "P22", "name": "Rose Pink", "price": 100, "pic": "rose_pink.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "rose"},
+    {"id": "P23", "name": "Rose Red", "price": 100, "pic": "rose_red.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "rose"},
+    {"id": "P24", "name": "Rose White", "price": 100, "pic": "rose_white.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "rose"},
+    {"id": "P25", "name": "Rose Yellow", "price": 100, "pic": "rose_yellow.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "rose"},
+    {"id": "P31", "name": "Sun", "price": 100, "pic": "sun.jpg", "description":"Biểu trưng cho một tình yêu đẹp mới chớm nở giữa hai người. Ý nghĩa hoa hồng hồng còn thay đổi theo độ đậm nhạt của hoa. Một bó hoa hồng nhạt vừa có thể bày tỏ tấm lòng biết ơn, sự cảm ơn vừa thể hiện sự ngưỡng mộ của bạn dành cho đối phương.", "cat": "sun"}
+];
+
+//goi ham displayImages()
+displayImages(data);
+
+//hien thi san pham
+function displayImages(items){
+    var s = ``;
+
+    $.each(items, function(k,v){
+        s += `<div class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">${v.name}</div>
+                    <div class="card-body"><a href="product_${v.id}.html"><img src="../image/${v.pic}" alt="${v.name}"></a></div> 
+                    <div class="card-footer">Price: ${v.price} <br>
+                        <button>Add to cart </button></div>
+                </div>
+            </div>`;
+    });
+
+    $("#products").html(s);
+}
+
+//lap trinh su kien search
+$("#formSearch").submit(function(e){
+    e.preventDefault();
+
+    var value = $("#search").val();
+    var re = new RegExp(value, "ig");
+    var subdata = data.filter(item => item.name.search(re) >= 0);
+    displayImages(subdata);
+})
+
+//lap trinh su kien click chon loai san pham
+$(".ck-flower").click(function(){
+    var category = $(".ck-flower:checked").map(function(){return $(this).val()}).toArray().toString();
+    var subdata = (category.length==0)?data : data.filter(item =>category.search(item.cat) >=0);
+    displayImages(subdata);
+})
+
+//show detail san pham
+function showProduct(pid){
+    let products = data.filter(item => item.id == pid);
+    let product = products[0];
+
+    let x = `<div class="col-sm-6"><img src="../image/${product.pic}" alt="${product.name}" width="500px"></div>
+            <div class="col-sm-6">
+                <h1>Name: ${product.name}</h1>
+                <h1>Price: ${product.price}</h1>
+                <h2>Description: ${product.description}</h2>
+                <button class="btn btn-primary">Add to cart</button>
+            </div>`;
+    $("#productDetail").html(x);
+}
